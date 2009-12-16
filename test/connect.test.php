00b42885b81148145626f2706471d20dcd4b04ce
Michael Granados <michaelgranados@gmail.com>
2009-12-15 22:44:41 -0200

    Dando include no arquivo model

--- test/connect.test.php
+++ test/connect.test.php
@@ -3,11 +3,12 @@
 
 # A biblioteca PHPUnit deve estar instalada para rodar os testes
 require_once('config.php');
+require_once('model.php');
 require_once('PHPUnit/Framework.php');
 
 class ConnectTest extends PHPUnit_Framework_TestCase
 {
-
+    
 }
 
 if (basename($_SERVER['SCRIPT_FILENAME']) === basename(__FILE__)) {
