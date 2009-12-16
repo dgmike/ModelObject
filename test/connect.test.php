9645c51ccc09a6119c079016cc55e7abdec5c7ee
Michael Granados <michaelgranados@gmail.com>
2009-12-15 22:43:52 -0200

    Criando o arquivo de testes de conexao

---
+++ test/connect.test.php
@@ -0,0 +1,16 @@
+<?php
+/* vim: set expandtab tabstop=4 shiftwidth=4 foldmethod=marker: */
+
+# A biblioteca PHPUnit deve estar instalada para rodar os testes
+require_once('config.php');
+require_once('PHPUnit/Framework.php');
+
+class ConnectTest extends PHPUnit_Framework_TestCase
+{
+
+}
+
+if (basename($_SERVER['SCRIPT_FILENAME']) === basename(__FILE__)) {
+    rodaTest('ConnectTest');
+}
+
