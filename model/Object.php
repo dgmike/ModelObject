60c71b86a0bbe68cebe8cfa3e17523b0edbd30f9
Michael Granados <michaelgranados@gmail.com>
2009-12-30 21:55:54 -0200

    convertendo a pasta para Model, em maiuscula, fazendo a busca pelo driver

--- model/Object.php
+++ model/Object.php
@@ -1,11 +1,38 @@
 <?php
+/* vim: set expandtab tabstop=4 shiftwidth=4 foldmethod=marker: */
+
+require_once('Interpretor.php');
 
 class Model_Object
 {
     public $_con = false;
+    public $_interoretor = false;
 
     public function __construct($dns, $user=null, $pass=null)
     {
-        $this->_con = new PDO($dns, $user, $pass);
+        $this->_interpretor = $this->getInterpretor($dns);
+        $this->_con  = new PDO($dns, $user, $pass);
+    }
+    
+    public function getInterpretor ($dns)
+    {
+        $interpretor = 'interpretor_'.reset(explode(':', $dns));
+        $interpretor = str_replace('_', ' ', $interpretor);
+        $interpretor = ucwords(strtolower($interpretor));
+        $interpretor = str_replace(' ', DIRECTORY_SEPARATOR, $interpretor);
+        // get the file
+        $file        = realpath(dirname(__FILE__));
+        $file       .= DIRECTORY_SEPARATOR.$interpretor.'.php';
+        if (!file_exists($file)) {
+            throw new Exception ('Interpretor file not found: '.$file);
+        }
+        require_once($file);
+        // Sets the interepretor
+        $interpretor = 'Model_'
+                       .str_replace(DIRECTORY_SEPARATOR, '_', $interpretor);
+        if (!class_exists($interpretor)) {
+            throw new Exception ('Interpretor class not found: '.$interpretor);
+        }
+        return new $interpretor;
     }
 }
--- test/connect.unittest.php
+++ test/connect.unittest.php
@@ -25,20 +25,48 @@ class ConnectTest extends PHPUnit_Framework_TestCase
         }
     }
 
+    public function _dns($type)
+    {
+        if ($type == 'sqlite')
+            return 'sqlite:banco.db';
+        if ($type == 'mysql')
+            return sprintf('mysql:host=%s;dbname=%s', TEST_MYSQL_DATAHOST, TEST_MYSQL_DATANAME);
+    }
+
     public function testSQLite()
     {
-        $con = new Model_Object('sqlite:banco.db');
+        $con = new Model_Object($this->_dns('sqlite'));
         $this->assertFileExists('banco.db');
+        $this->assertEquals('object', gettype($con->_con));
+        $this->assertEquals('PDO', get_class($con->_con));
     }
 
     public function testMySQL()
     {
-        $dns = sprintf('mysql:host=%s;dbname=%s', TEST_MYSQL_DATAHOST, TEST_MYSQL_DATANAME);
-        $con = new Model_Object($dns, TEST_MYSQL_USERNAME, TEST_MYSQL_PASSWORD);
+        $con = new Model_Object($this->_dns('mysql'), TEST_MYSQL_USERNAME, TEST_MYSQL_PASSWORD);
+        $this->assertEquals('object', gettype($con->_con));
+        $this->assertEquals('PDO', get_class($con->_con));
+    }
+
+    public function testInterpretor()
+    {
+        /*
+        $con = new Model_Object($this->_dns('mysql'), TEST_MYSQL_USERNAME, TEST_MYSQL_PASSWORD);
+        $this->assertEquals('Model_Interpretor_Mysql', get_class($con->_interpretor));
+        $con2 = new Model_Object($this->_dns('sqlite'));
+        $this->assertEquals('Model_Interpretor_Sqlite', get_class($con2->_interpretor));
+        */
+    }
+
+    public function testNotInterpretor ()
+    {
+        $ar = (get_class_methods($this)); sort($ar); print join("\n", $ar);
+        $file = realpath('../model/Interpretor'.DIRECTORY_SEPARATOR.'invalidDriver.php');
+        $this->setExpectedException('Interpretor file not found: '.$file);
+        $con = new Model_Object('invalidDriver:localhost', 'username', 'password');
     }
 }
 
 if (basename($_SERVER['SCRIPT_FILENAME']) === basename(__FILE__)) {
     rodaTest('ConnectTest');
 }
-
